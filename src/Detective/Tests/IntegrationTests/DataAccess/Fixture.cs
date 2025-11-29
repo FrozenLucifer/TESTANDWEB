13de7d548964331ad04efeaeaecf1860f64744ff
FrozenLucifer <andrey.12.56.34@gmail.com>
2025-11-29 17:20:02 +0300

    zlo

--- src/Detective/Tests/IntegrationTests/DataAccess/Fixture.cs
+++ src/Detective/Tests/IntegrationTests/DataAccess/Fixture.cs
@@ -11,10 +11,14 @@ public class PostgresDatabaseFixture : DatabaseFixtureBase
 
     public override async Task InitializeAsync()
     {
-        var host = Environment.GetEnvironmentVariable("POSTGRES_HOST") ?? "localhost";
+        // var host = Environment.GetEnvironmentVariable("POSTGRES_HOST") ?? "localhost";
+        // _testDbName = $"TestDb_{DateTime.Now:MMddHHmmss}_{Guid.NewGuid():N}";
+        // var connectionString = $"Host={host};Database={_testDbName};Username=postgres;Password=1";
+        // var host = Environment.GetEnvironmentVariable("POSTGRES_HOST") ?? "localhost";
         _testDbName = $"TestDb_{DateTime.Now:MMddHHmmss}_{Guid.NewGuid():N}";
-        var connectionString = $"Host={host};Database={_testDbName};Username=postgres;Password=1";
-        
+        // var connectionString = $"Host=localhost;Database={_testDbName};Username=postgres;Password=1;Port=5432";
+        var connectionString = $"Host=postgres;Database={_testDbName};Username=postgres;Password=1";
+
         var options = new DbContextOptionsBuilder<Context>()
             .UseNpgsql(connectionString)
             .UseExceptionProcessor()
